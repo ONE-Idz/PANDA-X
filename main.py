cf979eb76c3ef3cc1178b883b6e7cc3919df6f09
Jun pakaya <110225865+ONE-Idz@users.noreply.github.com>
2023-10-20 22:09:53 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,34 @@
+from scmusic import jun__ganteng__bgt
+import os, sys
+sys.stdout.write('\x1b]2;PANDA-X 8.0 VERSION\x07')
+try:os.system('git pull')
+except:pass
+jun__ganteng__bgt.cuyy__()
+###-------- -------------------------------------->
+#     *LOGO PANDA-X*
+###----------------------------------------------->
+def lologoku():
+	if "linux" in sys.platform.lower():
+		try:os.system('clear')
+		except:pass
+	elif "win" in sys.platform.lower():
+	    try:os.system('cls')
+	    except:pass
+	else:
+	    try:os.sytem('clear')
+	    except:pass
+	print("")
+	au="""
+██████╗  █████╗ ███╗    ██╗██████╗  █████╗ ██╗  ██╗
+██╔══██╗██╔══██╗████╗  ██║██╔══██╗██╔══██╗╚██╗██╔╝
+██████╔╝███████║██╔██╗ ██║██║  ██║███████║ ╚███╔╝   
+██╔═══╝ ██╔══██║██║╚██╗██║██║  ██║██╔══██║ ██╔██╗ 
+██║      ██║  ██║██║ ╚████║██████╔╝██║  ██║██╔╝ ██╗
+╚═╝      ╚═╝  ╚═╝╚═╝  ╚═══╝╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝ 
+Copyright By Jun.Pakaya 06 2023 ● ● ●"""
+	junId=nel(au,style="#FFFF00")
+	cetak(nel(junId, title='>>> ( [#00FF00]Banner ) <<<'))
+###----------------------------------------------->
+#     *JANGGAN DI PREMIN YA BANGSAD:)
+###----------------------------------------------->
+#*Thanks To Jun Pakaya*
